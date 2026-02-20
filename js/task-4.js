ce4f922676aeb880c3c29de1be378c012463d03c
Yana_Levchenko <yanalevchenko@gmail.com>
2026-02-20 22:30:44 +0100

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -3,6 +3,9 @@ const getTotalBalanceByGender = (users, gender) =>
     .filter(user => user.gender === gender)
     .map(user => user.balance)
     .reduce((total, balance) => total + balance, 0);
+
+
+
     const clients = [
 	{
     name: "Moore Hensley",
